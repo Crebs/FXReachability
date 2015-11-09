fcad11d9b0be05cb19033c8859f44e3b624b4179
Riley Crebs <rcrebs@salesforce.com>
2015-11-09 11:56:33 -0800

    Adding carthage support

---
+++ WSFXReachability/WSFXReachability.h
@@ -0,0 +1,20 @@
+//
+//  WSFXReachability.h
+//  WSFXReachability
+//
+//  Created by Riley Crebs on 11/9/15.
+//  Copyright © 2015 Incravo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WSFXReachability.
+FOUNDATION_EXPORT double WSFXReachabilityVersionNumber;
+
+//! Project version string for WSFXReachability.
+FOUNDATION_EXPORT const unsigned char WSFXReachabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSFXReachability/PublicHeader.h>
+#import <WSFXReachability/FXReachability.h>
+
+
